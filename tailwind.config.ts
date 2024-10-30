1c3edafa9b4b07cd40c2f697dafbf361b5cb097c
Pejezb <ronal2022quiroz@gmail.com>
2024-10-30 15:20:59 -0500

    Graficos y tarjetas - Admin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        pejeSky: "#C3EBFA",
+        pejeSky_Light: "#EDF9FD",
+        pejePurple: "#CFCEFF",
+        pejePurpleLight: "#F1F0FF",
+        pejeYellow: "#FAE27C",
+        pejeYellowLight: "#FEFCE8",
+    }    
     },
   },
   plugins: [],

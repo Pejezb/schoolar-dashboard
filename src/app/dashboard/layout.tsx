a209639187db5354fc7742c6bee18444d66ad329
Pejezb <ronal2022quiroz@gmail.com>
2024-10-30 14:15:51 -0500

    Ventana de navegacion

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Image from 'next/image';
 
@@ -9,7 +10,7 @@ export default function DashboardLayout({
   }>) {
     return <div className="h-screen flex">
         {/* LEFT */}
-        <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4'>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
           <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
               <Image src="/logo.png" alt="logo" width={32} height={32}/>
               <span className="hidden lg:block">Eduacacion Digital </span>
@@ -17,7 +18,13 @@ export default function DashboardLayout({
               <Menu/>
         </div>
         {/* RIGHT */}
-        <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]'></div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+
+        <Navbar />
+        {children}
+        
+        </div>
+          
 
         </div>;
 
